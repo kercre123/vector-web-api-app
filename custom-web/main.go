1a1472a0f026c4573f81516c59b0f7cba6a41583
Wire <whyyoulittle333@gmail.com>
2022-11-16 21:51:38 -0600

    Add things i dont remember

--- custom-web/main.go
+++ custom-web/main.go
@@ -5,10 +5,13 @@ import (
 	"context"
 	"crypto/tls"
 	b64 "encoding/base64"
+	"encoding/binary"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"image"
+	"image/color"
+	"io"
 	"io/ioutil"
 	"log"
 	"math/rand"
@@ -21,6 +24,7 @@ import (
 
 	"github.com/digital-dream-labs/vector-go-sdk/pkg/vector"
 	"github.com/digital-dream-labs/vector-go-sdk/pkg/vectorpb"
+	"github.com/fogleman/gg"
 	"github.com/gorilla/websocket"
 	"hz.tools/mjpeg"
 )
@@ -181,6 +185,121 @@ func moveHead(speed float32) {
 
 func releaseBehaviorControl() {
 	bcAssumption = false
+}
+
+func convertPixesTo16BitRGB(r uint32, g uint32, b uint32, a uint32) uint16 {
+	R, G, B := int(r/257), int(g/257), int(b/257)
+
+	return uint16((int(R>>3) << 11) |
+		(int(G>>2) << 5) |
+		(int(B>>3) << 0))
+}
+
+func convertPixelsToRawBitmap(image image.Image) []uint16 {
+	imgHeight, imgWidth := image.Bounds().Max.Y, image.Bounds().Max.X
+	bitmap := make([]uint16, 184*96)
+
+	for y := 0; y < imgHeight; y++ {
+		for x := 0; x < imgWidth; x++ {
+			bitmap[(y)*184+(x)] = convertPixesTo16BitRGB(image.At(x, y).RGBA())
+		}
+	}
+	return bitmap
+}
+
+func TextOnImg(text string, size float64) []byte {
+	bgImage := image.NewRGBA(image.Rectangle{
+		Min: image.Point{X: 0, Y: 0},
+		Max: image.Point{X: 184, Y: 96},
+	})
+	imgWidth := bgImage.Bounds().Dx()
+	imgHeight := bgImage.Bounds().Dy()
+	dc := gg.NewContext(imgWidth, imgHeight)
+	dc.DrawImage(bgImage, 0, 0)
+
+	if err := dc.LoadFontFace("/data/test.ttf", size); err != nil {
+		fmt.Println(err)
+		return nil
+	}
+
+	x := float64(imgWidth / 2)
+	y := float64((imgHeight / 2))
+	maxWidth := float64(imgWidth) - 35.0
+	dc.SetColor(color.White)
+	dc.DrawStringWrapped(text, x, y, 0.5, 0.5, maxWidth, 1.5, gg.AlignCenter)
+	buf := new(bytes.Buffer)
+	bitmap := convertPixelsToRawBitmap(dc.Image())
+	for _, ui := range bitmap {
+		binary.Write(buf, binary.LittleEndian, ui)
+	}
+	os.WriteFile("/tmp/test.raw", buf.Bytes(), 0644)
+	return buf.Bytes()
+}
+
+func imgOnFace(text string, size float64) {
+	faceBytes := TextOnImg(text, size)
+	_, _ = robot.Conn.DisplayFaceImageRGB(
+		ctx,
+		&vectorpb.DisplayFaceImageRGBRequest{
+			FaceData:         faceBytes,
+			DurationMs:       5000,
+			InterruptRunning: true,
+		},
+	)
+}
+
+func playSound(buf []byte, filename string) string {
+	os.WriteFile("/tmp/"+strings.TrimSpace(filename), buf, 0644)
+	var pcmFile []byte
+	if strings.Contains(filename, ".pcm") || strings.Contains(filename, ".raw") {
+		fmt.Println("Assuming already pcm")
+		pcmFile, _ = os.ReadFile("/tmp/" + strings.TrimSpace(filename))
+	} else {
+		conOutput, conError := exec.Command("ffmpeg", "-y", "-i", "/tmp/"+strings.TrimSpace(filename), "-f", "s16le", "-acodec", "pcm_s16le", "-ar", "16000", "-ac", "1", "/tmp/output.pcm").Output()
+		if conError != nil {
+			fmt.Println(conError)
+			return conError.Error()
+		}
+		fmt.Println("FFMPEG output: " + string(conOutput))
+		pcmFile, _ = os.ReadFile("/tmp/output.pcm")
+	}
+	var audioChunks [][]byte
+	for len(pcmFile) >= 1024 {
+		audioChunks = append(audioChunks, pcmFile[:1024])
+		pcmFile = pcmFile[1024:]
+	}
+	var audioClient vectorpb.ExternalInterface_ExternalAudioStreamPlaybackClient
+	audioClient, _ = robot.Conn.ExternalAudioStreamPlayback(
+		ctx,
+	)
+	audioClient.SendMsg(&vectorpb.ExternalAudioStreamRequest{
+		AudioRequestType: &vectorpb.ExternalAudioStreamRequest_AudioStreamPrepare{
+			AudioStreamPrepare: &vectorpb.ExternalAudioStreamPrepare{
+				AudioFrameRate: 16000,
+				AudioVolume:    100,
+			},
+		},
+	})
+	fmt.Println(len(audioChunks))
+	for _, chunk := range audioChunks {
+		audioClient.SendMsg(&vectorpb.ExternalAudioStreamRequest{
+			AudioRequestType: &vectorpb.ExternalAudioStreamRequest_AudioStreamChunk{
+				AudioStreamChunk: &vectorpb.ExternalAudioStreamChunk{
+					AudioChunkSizeBytes: 1024,
+					AudioChunkSamples:   chunk,
+				},
+			},
+		})
+		time.Sleep(time.Millisecond * 30)
+	}
+	audioClient.SendMsg(&vectorpb.ExternalAudioStreamRequest{
+		AudioRequestType: &vectorpb.ExternalAudioStreamRequest_AudioStreamComplete{
+			AudioStreamComplete: &vectorpb.ExternalAudioStreamComplete{},
+		},
+	})
+	os.Remove("/tmp/" + strings.TrimSpace(filename))
+	os.Remove("/tmp/output.pcm")
+	return "success"
 }
 
 func skipOnboarding() {
@@ -723,6 +842,28 @@ func apiHandler(w http.ResponseWriter, r *http.Request) {
 		speed, _ := strconv.Atoi(r.FormValue("speed"))
 		moveHead(float32(speed))
 		fmt.Fprintf(w, "")
+		return
+	case r.URL.Path == "/api/img_on_face":
+		text := r.FormValue("text")
+		sizeInt, _ := strconv.Atoi(r.FormValue("size"))
+		if text == "" {
+			text = "test"
+		}
+		size := float64(sizeInt)
+		fmt.Println(size)
+		imgOnFace(text, size)
+		fmt.Fprintf(w, "done :)")
+		return
+	case r.URL.Path == "/api/play_sound":
+		var buf bytes.Buffer
+		file, header, err := r.FormFile("file")
+		if err != nil {
+			fmt.Fprintf(w, "error")
+			return
+		}
+		io.Copy(&buf, file)
+		playSound(buf.Bytes(), header.Filename)
+		fmt.Fprintf(w, "uploaded")
 		return
 	case r.URL.Path == "/api/sound_version":
 		version := r.FormValue("version")
--- go.mod
+++ go.mod
@@ -4,8 +4,13 @@ go 1.14
 
 require (
 	github.com/digital-dream-labs/vector-go-sdk v0.0.0-20210319174132-8b9dd21232b6
+	github.com/faiface/beep v1.1.0 // indirect
+	github.com/fogleman/gg v1.3.0
+	github.com/go-audio/wav v1.1.0 // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/gorilla/websocket v1.5.0
 	github.com/grpc-ecosystem/grpc-gateway v1.16.0 // indirect
+	golang.org/x/image v0.1.0 // indirect
 	hz.tools/mjpeg v0.0.3
 )
